56945659beb755887884a4a6bbb5f0f6f1b673b5
Steven Berg <sjbergca@gmail.com>
2025-06-30 11:25:44 -0400

    remove Map Summary Table Header

--- app.py
+++ app.py
@@ -97,6 +97,7 @@ app.layout = html.Div([
             dcc.Graph(id='map-game-count'),
             dcc.Graph(id='map-winrate'),
             dcc.Graph(id='map-avg-score'),
+            """
             html.H4("Map Summary Table", style={'marginTop': '30px'}),
             dash_table.DataTable(
                 id='map-summary-table',
@@ -107,6 +108,7 @@ app.layout = html.Div([
                 style_cell={'textAlign': 'center'},
                 style_header={'fontWeight': 'bold'}
             ),
+            """
             html.Br(),
             html.H4("Corporation Performance by Map (Win %)", style={'marginTop': '40px'}),
             html.Div(id='corp-map-summary-table')         
